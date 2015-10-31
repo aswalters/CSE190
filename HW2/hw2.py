aa92adea60206c2fdd8f72d954f80a8247851ef0
NolanHartwick <nhartwic@ucsd.edu>
2015-10-31 15:42:02 -0700

    working network

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -48,45 +48,52 @@ def load_mnist(dataset="training", digits=numpy.arange(10), path="."):
     return images, labels
 
 
-def normalize_data(data, pca_flag=False):
+def normalize_data(data):
     data = [numpy.concatenate(x) for x in data]
     data = numpy.array([zscore(x) for x in data])
-    if (pca_flag):
-        pca = PCA(20)
-        data = pca.fit_transform(data)
     return data
 
 
-def get_data(t,pca_flag=False):
+def get_data(t):
     if (t == 'test'):
         images, targets = load_mnist(dataset='testing', path='../HW1/Regression')
-        # images = images[:2000]
-        # targets = targets[:2000]
-        images = normalize_data(images,pca_flag)
+        images = normalize_data(images)
         targets = numpy.array([[int(t[0] == i) for i in range(10)] for t in targets])
         return images, targets
     if (t == 'train'):
         images, targets = load_mnist(dataset='training', path='../HW1/Regression')
-        # images = images[:20000]
-        # targets = targets[:20000]
-        images = normalize_data(images,pca_flag)
+        images = normalize_data(images)
         targets = numpy.array([[int(t[0] == i) for i in range(10)] for t in targets])
         return images, targets
-    raise Exception('You broken it...')
+    raise Exception('You brokeded it...')
 
 
-def HLN_tester():
-    images, targets = get_data('train')
-    machine = HLN(images, targets, hidden=(100, 1),patience=1000)
-    images,targets = get_data('test')
+def fake_data():
     max_index = lambda x : max([(i,e) for i,e in enumerate(x)],key=lambda y : y[1])[0]
+    X = 4
+    Y = 1000
+    data = [numpy.array([random.random() for x in range(X)]) for y in range(Y)]
+    targets = [max_index(x) for x in data]
+    targets = numpy.array([[int(t == i) for i in range(X)] for t in targets])
+    return data,targets
+
+    
+def HLN_tester():
+    max_index = lambda x : max([(i,e) for i,e in enumerate(x)],key=lambda y : y[1])[0]
+    images, targets = get_data('train')
+    #images, targets = fake_data()
+    machine = HLN(images, targets,patience=5000,rate=10**-3,batch=50,hidden=[50])
+    for w in machine.weights:
+        print(w.shape)
+    images,targets = get_data('test')
     count = 0
-    for d,t in zip(images,targets):
+    s = len(images)
+    for d,t in zip(images[:s],targets[:s]):
         p = machine.predict(d)
         p = max_index(p[-1])
-        if(t[p]!=1):
+        if(t[p]==1):
             count+=1
-    print(count/len(images))
+    print('Accuracy : '+str(count/s))
 
 
 def softmax(vect):
@@ -95,106 +102,76 @@ def softmax(vect):
 
 
 class HLN:
-    def __init__(self, data, targets, rate=0.01, drop=0.5, patience=100000, batch=100, hidden=(10, 2),
-                 act_func=softmax):
-
+    def __init__(self, data, targets, rate=10**-4, drop=0.5, patience=100000, batch=100, hidden=[100],act_func=softmax):
+        norm = lambda scale: numpy.random.normal(scale=scale)
+        scale_func = lambda x : 1/numpy.sqrt(x+1)
+        layers = [len(data[0])]+list(hidden)+[len(targets[0])]
         self.act_func = act_func
-        self.I = len(data[0])
-        self.J, self.L = hidden
-        self.K = len(targets[0])
-        self.a = rate
+        self.rate = rate/batch
         self.weights = []
         self.bias = []
-
-        norm = lambda scale: numpy.random.normal(scale=scale)
-
-        scale = 1 / numpy.sqrt(self.I + 1)
-        initial_weights = numpy.array([[norm(scale) for x in range(self.I)] for y in range(self.J)])
-        J1_bias = numpy.array([norm(scale) for y in range(self.J)])
-
-        scale = 1 / numpy.sqrt(self.J + 1)
-        hidden_weights = numpy.array(
-            [[[norm(scale) for x in range(self.J)] for y in range(self.J)] for z in range(self.L - 1)])
-        JJ_bias = numpy.array([[norm(scale) for x in range(self.J)] for y in range(self.L - 1)])
-        final_weights = numpy.array([[norm(scale) for x in range(self.J)] for y in range(self.K)])
-        K_bias = numpy.array([norm(scale) for x in range(self.K)])
-
-        self.weights.append(initial_weights)
-        for w in hidden_weights:
-            self.weights.append(w)
-        self.weights.append(final_weights)
-
-        self.bias.append(J1_bias)
-        for b in JJ_bias:
-            self.bias.append(b)
-        self.bias.append(K_bias)
-
+        for i in range(1,len(layers)):
+            self.weights.append(numpy.matrix(
+                [[norm(scale_func(layers[i-1])) for x in range(layers[i-1])] for y in range(layers[i])]))
+            self.bias.append(numpy.matrix([norm(scale_func(layers[i-1])) for x in range(layers[i])]).transpose())
+        self.transpose_weights = [x.transpose() for x in self.weights]
         data = numpy.array([[d, t] for d, t in zip(data, targets)])
         numpy.random.shuffle(data)
-        count = 0
-        TRACK = 0
+        count=0
+        matrix_sum_square = lambda m : sum(x**2 for x in numpy.nditer(m))
         for _ in range(patience):
-
-            weight_update = [numpy.array([[0 for x in y] for y in z]) for z in self.weights]
-            bias_update = [numpy.array([0 for x in y]) for y in self.bias]
             if (count + batch > len(data)):
                 numpy.random.shuffle(data)
                 count = 0
+            weight_update = [numpy.matrix([[0 for x in y] for y in z]) for z in self.weights]
+            bias_update = [numpy.matrix([0 for x in y]).transpose() for y in self.bias]
             for i in range(batch):
-                i = count + i
-                if TRACK == 1:
-                    print("****")
-                    for b in self.bias:
-                        print(b.shape)
-                    print('****')
-                    print(i)
-                    print(len(data[i][0]))
-                    print('****')
+                i += count
                 prediction = self.predict(data[i][0])
                 wu, bu = self.update(prediction, data[i][1])
-
                 weight_update = [w_old + w_new for w_old, w_new in zip(weight_update, wu)]
-                bias_update = [b_old + b_new for b_old, b_new in zip(bias_update, bu)]
-
+                bias_update = [b_old + b_new for b_old, b_new in zip(bias_update, bu)]    
             count += batch
-
-            self.weights = [w_old - w_new for w_old, w_new in zip(self.weights, weight_update)]
-            self.bias = [b_old - b_new for b_old, b_new in zip(self.bias, bias_update)]
-
-            TRACK += 1
-
-
+            self.weights = [w_old + w_new for w_old, w_new in zip(self.weights, weight_update)]
+            self.transpose_weights = [x.transpose() for x in self.weights]
+            self.bias = [b_old + b_new for b_old, b_new in zip(self.bias, bias_update)]
 
     def predict(self, datum, activation=None):
         if (activation == None):
-            activation = lambda val: 1.7159 * numpy.tanh(2 / 3 * val)
+            activation = lambda val : 1.7159 * numpy.tanh(2 / 3 * val)
         outputs = []
-        outputs.append(datum)
+        outputs.append(numpy.matrix(datum).transpose())
+        count = 0
         for w, b in zip(self.weights, self.bias):
-            print('w : '+str(w.shape))
-            print('i : '+str(outputs[-1].shape))
-            outputs.append(activation(numpy.dot(w, outputs[-1]) + b))
-        outputs[-1] = self.act_func(numpy.dot(self.weights[-1], outputs[-2]) + self.bias[-1])
+            count+=1
+            if(len(self.weights)==count):
+                break
+            outputs.append(activation(numpy.dot(w,outputs[-1]))+b)
+        outputs.append(self.act_func(numpy.dot(self.weights[-1], outputs[-1]) + self.bias[-1]))
         return outputs
 
     def update(self, out, t):
         deltas = []
+        t = numpy.matrix(t).transpose()
         final_delta = t - out[-1]
         deltas.append(final_delta)
-        for w, o in zip(reversed(self.weights), reversed(out[:-1])):
-            deltas.append(numpy.dot(w.transpose(), deltas[-1]) * o)
-        deltas = deltas[:-1]
+        count = 0
+        for w, o in zip(reversed(self.transpose_weights), reversed(out[:-1])):
+            count+=1
+            if(count==len(self.weights)):
+                break
+            temp = numpy.dot(w,deltas[-1])
+            temp = numpy.multiply(temp,o)
+            deltas.append(temp)
         update = []
-        for w, d, o in zip(self.weights, reversed(deltas), out):
-            d = numpy.matrix(d)
-            o = numpy.matrix(o)
-            update.append(self.a * numpy.dot(d.transpose(), o))
-        bias_update = [self.a * d for d in reversed(deltas)]
-
+        for d, o in zip(reversed(deltas), out):
+            update.append(self.rate * numpy.dot(d, o.transpose()))
+        bias_update = [self.rate * d for d in reversed(deltas)]
         return update, bias_update
 
 
 if (__name__ == '__main__'):
+    print('Start Time : '+time.asctime())
     start = time.time()
     HLN_tester()
     stop = time.time()
